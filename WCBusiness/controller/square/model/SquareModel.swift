0c2f865a60a6284bad7ee89158f3898dc2b3d47e
yangxl <123456>
2017-10-13 10:16:26 +0800

    model

---
+++ WCBusiness/controller/square/model/SquareModel.swift
@@ -0,0 +1,14 @@
+//
+//  SquareModel.swift
+//  WCBusiness
+//
+//  Created by YangXL on 2017/10/12.
+//  Copyright © 2017年 LYL. All rights reserved.
+//
+
+import Foundation
+
+struct square {
+    var name:String?
+    var icon:String?
+}
