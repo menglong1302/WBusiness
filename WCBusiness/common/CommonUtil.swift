12f09b06b5bb972e53cac7849885fee6b344e4df
yangxl <123456>
2017-10-13 10:13:21 +0800

    common

---
+++ WCBusiness/common/CommonUtil.swift
@@ -0,0 +1,12 @@
+//
+//  CommonUtil.swift
+//  WCBusiness
+//
+//  Created by YangXL on 2017/10/12.
+//  Copyright © 2017年 LYL. All rights reserved.
+//
+
+import UIKit
+
+public let SCREEN_WIDTH=UIScreen.main.bounds.size.width
+public let SCREEN_HEIGHT=UIScreen.main.bounds.size.height
--- WCBusiness/controller/WBSquareViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WBSquareViewController.swift
-//  WCBusiness
-//
-//  Created by YangXL on 2017/10/11.
-//  Copyright © 2017年 LYL. All rights reserved.
-//
-
-import Foundation
