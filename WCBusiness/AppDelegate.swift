fc6071cf532b727307fd0f59c17251a65c067c9c
yangxl <123456>
2017-10-13 10:17:25 +0800

    tabbar rootVC

--- WCBusiness/AppDelegate.swift
+++ WCBusiness/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
  
         window = UIWindow.init(frame:UIScreen.main.bounds)
-        window?.rootViewController = WBMyViewController()
+        window?.rootViewController = WBTabBarController()
         window?.makeKeyAndVisible()
         return true
     }
