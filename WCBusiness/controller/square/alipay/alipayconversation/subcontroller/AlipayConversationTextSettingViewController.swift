1148c867d1779a7a79a2be7282a5c80a84686090
qq_19302739 <7585269@qq.com>
2017-10-26 10:12:10 +0800

    移动目录

--- WCBusiness/controller/square/alipay/alipayconversation/subcontroller/AlipayConversationTextSettingViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AlipayConversationTextSettingViewController.swift
-//  WCBusiness
-//
-//  Created by Ray on 2017/10/25.
-//  Copyright © 2017年 LYL. All rights reserved.
-//
-
-import Foundation
